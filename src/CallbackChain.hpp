15bcdc14dc8851daa2c0d82a44f7431e20f757bc
Zombori Dániel <rockerm4nhun@gmail.com>
2019-12-15 16:56:02 +0100

    Make CallbackChain header only

--- src/CallbackChain.hpp
+++ src/CallbackChain.hpp
@@ -11,3 +11,22 @@ protected:
     CallbackChain<ChainID, Args...>* next;
     func f;
 };
+
+// ----- Implementation -----
+
+template<typename ChainID, typename ... Args>
+CallbackChain<ChainID, Args...>* CallbackChain<ChainID, Args...>::last = nullptr;
+
+template<typename ChainID, typename ... Args>
+CallbackChain<ChainID, Args...>::CallbackChain(func f) : next(last), f(f){
+    last = this;
+}
+
+template<typename ChainID, typename ... Args>
+void CallbackChain<ChainID, Args...>::fire(Args... args){
+    CallbackChain<ChainID, Args...>* obj = last;
+    while(obj){
+        obj->f(args...);
+        obj = obj->next;
+    }
+}

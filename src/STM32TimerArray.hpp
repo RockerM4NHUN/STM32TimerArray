2842c6d84b54a22fb8e5fd686ce5eacd007f7715
Zombori Dániel <rockerm4nhun@gmail.com>
2019-12-22 15:10:00 +0100

    V0.2.0 interrupt safe version

--- src/STM32TimerArray.hpp
+++ src/STM32TimerArray.hpp
@@ -3,5 +3,5 @@
 #include "version.h"
 #include "TimerArrayControl.hpp"
 
-// Interrupt safe version (V0.1.0)
+// Interrupt safe version (V0.2.0)
 // LIGHTLY TESTED
--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 namespace STM32TimerArray
 {
 
-constexpr const char *version = "V0.1.0";
+constexpr const char *version = "V0.2.0";
 constexpr const char *branch = "master";
 
 } // namespace STM32TimerArray

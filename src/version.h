10fff73f97ab4a63c05d7e743c58b3d9b0816a4d
Zombori Dániel <rockerm4nhun@gmail.com>
2019-12-28 21:37:46 +0100

    Bump to V0.3.0

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 namespace STM32TimerArray
 {
 
-constexpr const char *version = "V0.2.0";
-constexpr const char *branch = "master";
+constexpr const char *version = "V0.3.0";
+constexpr const char *branch = "dev";
 
 } // namespace STM32TimerArray

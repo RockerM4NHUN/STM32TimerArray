ffe0b1ad942b60a7fdaab20010a8758718a8187d
Zombori Dániel <rockerm4nhun@gmail.com>
2020-01-03 17:07:38 +0100

    Adapt example to clkdiv bugfix

--- examples/blink_with_cubemx/app.cpp
+++ examples/blink_with_cubemx/app.cpp
@@ -9,15 +9,15 @@
 #include "STM32TimerArray.hpp"
 
 // Timer hardware's input frequency, set in code or CubeMX
-// In this example we use the CPU's frequency (what was set in CubeMX).
+// In this example we use the CPU's frequency (what was set in CubeMX by default).
 uint32_t timerInputFrequency = F_CPU;
 
 // The timers targeted frequency division, might not be achievable with the available hardware.
 // Divides the |timerInputFrequency|, the result is the controller's tick frequency.
 // To get the correct division, we divided the input frequency with the target frequency.
-// In this example the target frequency is 1 kHz or 1000 Hz.
+// In this example the target frequency is 10 kHz or 10000 Hz.
 // *** Warning: this is integer division, the result will be rounded towards negative infinity. ***
-uint32_t frequencyDivision = timerInputFrequency/1000;
+uint32_t frequencyDivision = timerInputFrequency/10000;
 
 // Number of usable bits for counting in the timer. Some timers have 16 bits, others have 32 bit counter register.
 // Using 16 bits for a 32 bit counter will be OK, but severly limits the maximum possible delay.
@@ -37,7 +37,7 @@ void toggle_led(){
 }
 
 Timer t_toggle(
-    500,       // the number of ticks to wait
+    5000,      // the number of ticks to wait
     true,      // isPeriodic, false means one shot, true means repeating
     toggle_led // pointer to the static callback function
 );
@@ -46,14 +46,14 @@ Timer t_toggle(
 void app_start(){
 	
 	
-    // Attach before begin is also valid.
+    // Attach before begin is also valid and a good way to synchronize timers at startup.
     // control.attachTimer(&t_toggle);
 
     // Start timer array configured to run at 1 kHz (tick frequency).
     control.begin();
 
     // Initiate half second toggle, meaning 1 Hz blinking.
-	// 1 ms (millisecond) per tick counting multiplied by 500 ticks is 500 ms or 0.5 sec.
+	// 0.1 ms (millisecond) per tick counting multiplied by 5000 ticks is 500 ms or 0.5 sec.
     control.attachTimer(&t_toggle);
 	
 	// Lay back, the timer callback is set, interrupts will handle everything.

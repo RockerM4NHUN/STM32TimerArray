3daad9fda6e9b125cb020b0f1da57be8f997fb7b
Zombori Dániel <rockerm4nhun@gmail.com>
2020-01-13 16:59:00 +0100

    Release v0.4.1

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 namespace STM32TimerArray
 {
 
-constexpr const char *version = "V0.4.0";
+constexpr const char *version = "V0.4.1";
 constexpr const char *branch = "master";
 
 } // namespace STM32TimerArray

f090da917e06a0165758ad9681d887944dc09ce4
Zombori Dániel <rockerm4nhun@gmail.com>
2020-01-15 22:26:38 +0100

    Refactor timers into Timer files

--- src/TimerArrayControl.hpp
+++ src/TimerArrayControl.hpp
@@ -7,69 +7,12 @@
 #include "stm32_hal.h"
 
 #include "CallbackChain.hpp"
+#include "Timer.hpp"
 
 
 // Callback chain setup for HAL_TIM_OC_DelayElapsedCallback function
 struct TIM_OC_DelayElapsed_CallbackChainID{};
 using TIM_OC_DelayElapsed_CallbackChain = CallbackChain<TIM_OC_DelayElapsed_CallbackChainID, TIM_HandleTypeDef*>;
-
-
-// Represents a timer, handled by a TimerArrayControl object.
-// Attach it to a controller to receive callbacks.
-//
-// delay: ticks of timer array controller until firing
-// periodic: does the timer restart immedietely when fires
-// f: static function called when timer is firing
-class Timer{
-public:
-    using callback_function = void(*)();
-    Timer(const callback_function f);
-    Timer(uint32_t delay, bool periodic, const callback_function f);
-    
-    bool isRunning();
-    bool isPeriodic();
-    uint32_t delay();
-
-    void periodic(bool val);
-    void delay(uint32_t val);
-
-    // Changing the timers delay will not affect the current firing event, only the next one.
-    // To restart the timer with the new delay, detach and attach it.
-    // To change the timer's delay without restart, use the changeTimerDelay method
-    // in the TimerArrayControl.
-
-protected:
-    uint32_t _delay; // required delay of timer (in ticks)
-    bool _periodic; // should the timer be immedietely restarted after firing
-    uint32_t target; // counter value that the timer fires at next
-    void *const f; // WARNING: unsafe if you force the call of a certain fire method instead of letting the inheritance decide
-    bool running;
-    Timer* next;
-
-    virtual void fire();
-
-    friend class TimerArrayControl;
-};
-
-// Represents a Timer with context.
-//
-// delay: ticks of timer array controller until firing
-// isPeriodic: does the timer restart immedietely when fires
-// ctx: context pointer to an object, will be passed to the callback function
-// ctxf: static function called when timer is firing, takes a Context* pointer argument
-template<typename Context>
-class ContextTimer : public Timer{
-public:
-    using dynamic_callback_function = void(*)(Context*);
-    ContextTimer(Context* ctx, const dynamic_callback_function ctxf) : Timer((callback_function)ctxf), ctx(ctx) {}
-    ContextTimer(uint32_t delay, bool isPeriodic, Context* ctx, const dynamic_callback_function ctxf) : Timer(delay, isPeriodic, (callback_function)ctxf), ctx(ctx) {}
-protected:
-    Context* ctx;
-
-    virtual void fire(){
-        ((dynamic_callback_function)f)(ctx);
-    }
-};
 
 
 // Implements timer controller for hardware handling,
